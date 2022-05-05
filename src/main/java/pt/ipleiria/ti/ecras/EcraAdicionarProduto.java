f65a0e46792bdbabf48db8780cd37f05428232f6
André Pereira <andremiguel.lp@gmail.com>
2022-05-05 16:42:20 +0100

    Alterações no ecrã adicionar - Inicio layout

--- src/main/java/pt/ipleiria/ti/ecras/EcraAdicionarProduto.java
+++ src/main/java/pt/ipleiria/ti/ecras/EcraAdicionarProduto.java
@@ -6,11 +6,24 @@ import javax.swing.*;
 
 public class EcraAdicionarProduto extends BaseScreen {
     private JPanel rootPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField4;
+    private JComboBox comboBox1;
+    private JComboBox comboBox2;
+    private JLabel NomeProduto;
+    private JLabel DescriçãoProduto;
+    private JLabel CategoriaProduto;
+    private JLabel PreçoProduto;
 
     public EcraAdicionarProduto(String windowTitle) {
         super(windowTitle);
 
         super.getScreen().setSize(800, 600);
         super.setupScreen(rootPanel);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
     }
 }
