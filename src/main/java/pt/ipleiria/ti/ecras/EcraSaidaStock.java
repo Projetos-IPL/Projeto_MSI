1eaa703b4d56358c0d6f525f67e25797eddb6de5
André Pereira <andremiguel.lp@gmail.com>
2022-05-05 17:29:59 +0100

    Criação ecrã saida de stock - inicio de layout

---
+++ src/main/java/pt/ipleiria/ti/ecras/EcraSaidaStock.java
@@ -0,0 +1,16 @@
+package pt.ipleiria.ti.ecras;
+
+import javax.swing.*;
+
+public class EcraSaidaStock {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JComboBox comboBox1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JLabel NomeProduto;
+    private JLabel DataProduto;
+    private JLabel LoteProduto;
+    private JComboBox comboBox2;
+    private JLabel QuantidadeProduto;
+}
